49e9861155007fc95230db7084e2302968980095
MathewsTheCoder007 <89633526+MathewsTheCoder007@users.noreply.github.com>
2022-01-17 13:18:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -122,7 +122,7 @@ const btn = document.querySelector('.talk');
 		if (message.includes('close')) {
 		    window.close(this);
 	     }
-		if(message.includes(SuperBot)) {
+		if(message.includes(uperbot)) {
 			window.open('https://mathewsthecoder007.github.io/SuperBot/');
 		}
             speech.volume = 1;
